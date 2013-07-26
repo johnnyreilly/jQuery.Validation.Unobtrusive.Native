b36fa8069ca60c1822348cabb98c5566edbe59c0
johnnyreilly <johnny_reilly@hotmail.com>
2013-07-26 22:05:08 +0100

    Started adding Range unit tests

--- jVNUMUnitTests/TextBoxExtensionsUnitTests.cs
+++ jVNUMUnitTests/TextBoxExtensionsUnitTests.cs
@@ -8,38 +8,6 @@ namespace jQueryValidateNativeUnobtrusiveMVCUnitTests
     [TestClass]
     public class TextBoxExtensionsUnitTests
     {
-        /*
-        private class RangeAndNumberModel
-        {
-            [Range(-20, 40)]
-            public decimal RangeAndNumberDemo { get; set; }
-        }
-
-        /// <summary>
-        /// Test for @Html.TextBoxFor(x => x.RangeAndNumberDemo, true)
-        /// </summary>
-        [TestMethod]
-        public void TextBoxFor_has_range_and_number_data_attributes()
-        {
-            // Arrange
-            var htmlHelper = HtmlHelperFactory.Create(new RangeAndNumberModel { RangeAndNumberDemo = 77 });
-
-            // Act
-            var result = TextBoxExtensions.TextBoxFor(htmlHelper, exampleModel => exampleModel.RangeAndNumberDemo, true);
-
-            // Assert
-            Assert.AreEqual("<input " +
-                "data-msg-number=\"The field RangeAndNumberDemo must be a number.\" " +
-                "data-msg-range=\"The field RangeAndNumberDemo must be between -20 and 40.\" " +
-                "data-msg-required=\"The RangeAndNumberDemo field is required.\" " +
-                "data-rule-number=\"true\" " +
-                "data-rule-range=\"[-20,40]\" " +
-                "data-rule-required=\"true\" " +
-                "id=\"RangeAndNumberDemo\" name=\"RangeAndNumberDemo\" type=\"text\" value=\"\" />", 
-                result.ToHtmlString());
-        }
-        */
-
         #region Number tests for @Html.TextBoxFor(x => x.Number, true)
 
         private const string HTMLRequiredNumber = "<input " +
@@ -378,5 +346,90 @@ namespace jQueryValidateNativeUnobtrusiveMVCUnitTests
         
         #endregion
 
+        #region Range tests for @Html.TextBoxFor(x => x.Number, true)
+
+        private const string HTMLRangeRequiredNumber = "<input " +
+                                                       "data-msg-number=\"The field Number must be a number.\" " +
+                                                       "data-msg-range=\"The field Number must be between -20 and 40.\" " +
+                                                       "data-msg-required=\"The Number field is required.\" " +
+                                                       "data-rule-number=\"true\" " +
+                                                       "data-rule-range=\"[-20,40]\" " +
+                                                       "data-rule-required=\"true\" " +
+                                                       "id=\"Number\" name=\"Number\" type=\"text\" value=\"\" />";
+
+        private const string HTMLRangeRequiredNumberWithFraction = "<input " +
+                                                                   "data-msg-number=\"The field Number must be a number.\" " +
+                                                                   "data-msg-range=\"The field Number must be between -20.5 and 40.3.\" " +
+                                                                   "data-msg-required=\"The Number field is required.\" " +
+                                                                   "data-rule-number=\"true\" " +
+                                                                   "data-rule-range=\"[-20.5,40.3]\" " +
+                                                                   "data-rule-required=\"true\" " +
+                                                                   "id=\"Number\" name=\"Number\" type=\"text\" value=\"\" />";
+
+        private const string HTMLRangeNumberWithFraction = "<input " +
+                                                           "data-msg-number=\"The field Number must be a number.\" " +
+                                                           "data-msg-range=\"The field Number must be between -20.5 and 40.3.\" " +
+                                                           "data-rule-number=\"true\" " +
+                                                           "data-rule-range=\"[-20.5,40.3]\" " +
+                                                           "id=\"Number\" name=\"Number\" type=\"text\" value=\"\" />";
+        
+        private class ShortRangeModel
+        {
+            [Range(typeof(short), "-20", "40")]
+            public short Number { get; set; }
+        }
+
+        [TestMethod]
+        public void TextBoxFor_has_range_required_and_number_data_attributes_for_short_with_Range_attribute()
+        {
+            // Arrange
+            var htmlHelper = HtmlHelperFactory.Create(new ShortRangeModel());
+
+            // Act
+            var result = TextBoxExtensions.TextBoxFor(htmlHelper, exampleModel => exampleModel.Number, true);
+
+            // Assert
+            Assert.AreEqual(HTMLRangeRequiredNumber, result.ToHtmlString());
+        }
+
+        private class DecimalRangeModel
+        {
+            [Range(typeof(decimal), "-20.5", "40.3")]
+            public decimal Number { get; set; }
+        }
+
+        [TestMethod]
+        public void TextBoxFor_has_range_required_and_number_data_attributes_for_decimal_with_Range_attribute()
+        {
+            // Arrange
+            var htmlHelper = HtmlHelperFactory.Create(new DecimalRangeModel());
+
+            // Act
+            var result = TextBoxExtensions.TextBoxFor(htmlHelper, exampleModel => exampleModel.Number, true);
+
+            // Assert
+            Assert.AreEqual(HTMLRangeRequiredNumberWithFraction, result.ToHtmlString());
+        }
+
+        private class NullableDecimalRangeModel
+        {
+            [Range(typeof(decimal), "-20.5", "40.3")]
+            public decimal? Number { get; set; }
+        }
+
+        [TestMethod]
+        public void TextBoxFor_has_range_and_number_data_attributes_for_decimal_with_Range_attribute()
+        {
+            // Arrange
+            var htmlHelper = HtmlHelperFactory.Create(new NullableDecimalRangeModel());
+
+            // Act
+            var result = TextBoxExtensions.TextBoxFor(htmlHelper, exampleModel => exampleModel.Number, true);
+
+            // Assert
+            Assert.AreEqual(HTMLRangeNumberWithFraction, result.ToHtmlString());
+        }
+
+        #endregion
     }
 }
