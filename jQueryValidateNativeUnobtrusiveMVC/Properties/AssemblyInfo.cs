969ee8ae1ac01f30fecbf87cb3abc2d2c63513d4
John Reilly <johnny_reilly@hotmail.com>
2013-07-29 15:19:10 +0100

    Published v0.1.0 to Nuget

--- jQueryValidateNativeUnobtrusiveMVC/Properties/AssemblyInfo.cs
+++ jQueryValidateNativeUnobtrusiveMVC/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("jQueryValidateNativeUnobtrusiveMVC")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MVC HTML helper extensions that make use of jQuery Validation's native unobtrusive support for validation driven by HTML 5 data attributes.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("John Reilly")]
 [assembly: AssemblyProduct("jQuery Validate Native Unobtrusive MVC")]
-[assembly: AssemblyCopyright("Copyright © John Reilly 2013")]
+[assembly: AssemblyCopyright("Copyright © 2013 John Reilly (johnny_reilly@hotmail.com)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
