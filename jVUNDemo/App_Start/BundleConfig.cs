d31d9baedce5364dc29fdd8dc5618570e5735ef6
John Reilly <johnny_reilly@hotmail.com>
2013-08-01 14:19:23 +0100

    Added JavaScript tab to demo

--- jVUNDemo/App_Start/BundleConfig.cs
+++ jVUNDemo/App_Start/BundleConfig.cs
@@ -11,8 +11,6 @@ namespace jQuery.Validation.Unobtrusive.Native.Demos.App_Start
 
             bundles.Add(new ScriptBundle("~/bundles/jquery-validation").Include("~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/validate-form").Include("~/Scripts/validate-form.js"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
