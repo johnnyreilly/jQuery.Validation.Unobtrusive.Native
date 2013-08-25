68264dd3dc6e37e333df181b8817e13e200737d7
johnnyreilly <johnny_reilly@hotmail.com>
2013-08-25 09:16:09 +0100

    Adding overriding pending changes unit test

--- jVUNUnitTests/TextBoxExtensionsUnitTests.cs
+++ jVUNUnitTests/TextBoxExtensionsUnitTests.cs
@@ -713,8 +713,29 @@ namespace jQueryValidateNativeUnobtrusiveMVCUnitTests
             var result = TextBoxExtensions.TextBoxFor(htmlHelper, exampleModel => exampleModel.String, true, htmlAttributes: new { data_test_attribute = "I am a test" });
 
             // Assert
-            Assert.AreEqual("<input "+
-                "data-test-attribute=\"I am a test\" "+
+            Assert.AreEqual("<input " +
+                "data-test-attribute=\"I am a test\" " +
+                "id=\"String\" name=\"String\" type=\"text\" value=\"\" />", result.ToHtmlString());
+        }
+
+        private class RequiredStringThatWillBeOverriddenModel
+        {
+            [Required]
+            public string String { get; set; }
+        }
+
+        [TestMethod]
+        public void TextBoxFor_overrides_generated_data_attributes_with_custom_data_attributes()
+        {
+            // Arrange
+            var htmlHelper = HtmlHelperFactory.Create(new RequiredStringThatWillBeOverriddenModel()); // used this model as it has few generated metadata attributes
+
+            // Act
+            var result = TextBoxExtensions.TextBoxFor(htmlHelper, exampleModel => exampleModel.String, true, htmlAttributes: new { data_rule_required = "Yup", data_msg_required = "Uh-huh" });
+
+            // Assert
+            Assert.AreEqual("<input " +
+                "data-msg-required=\"Uh-huh\" data-rule-required=\"Yup\" " +
                 "id=\"String\" name=\"String\" type=\"text\" value=\"\" />", result.ToHtmlString());
         }
 
