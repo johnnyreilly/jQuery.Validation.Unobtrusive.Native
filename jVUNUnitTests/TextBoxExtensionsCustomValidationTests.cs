b3b7dde14a1568ec7d1e124a676020f2ab1c9173
johnnyreilly <johnny_reilly@hotmail.com>
2013-09-16 20:18:44 +0100

    Added custom validation test

---
+++ jVUNUnitTests/TextBoxExtensionsCustomValidationTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace jQuery.Validation.Unobtrusive.Native.MVC.UnitTests
+{
+    /// <summary>
+    /// Custom attribute used only for testing purposes
+    /// </summary>
+    public class CustomAttribute : ValidationAttribute, IClientValidatable
+    {
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            var rule = new ModelClientValidationRule
+            {
+                ErrorMessage = ErrorMessage,
+                ValidationType = "notequalto"
+            };
+            rule.ValidationParameters["other"] = "#" + OtherProperty;
+            yield return rule;
+        }
+
+        public string OtherProperty { get; private set; }
+
+        public CustomAttribute(string otherProperty)
+        {
+            OtherProperty = otherProperty;
+        }
+
+    }
+    [TestClass]
+    public class TextBoxExtensionsCustomValidationTests
+    {
+        public class CustomValidationModel
+        {
+            public string Property { get; set; }
+
+            [Custom("Property", ErrorMessage = "These fields cannot match")]
+            public string DifferentProperty { get; set; }
+        }
+
+
+        [TestMethod]
+        public void TextBoxFor_creates_custom_validation_data_attributes_for_string_with_custom_attribute()
+        {
+            // Arrange
+            var htmlHelper = HtmlHelperFactory.Create(new CustomValidationModel());
+
+            // Act
+            var result = TextBoxExtensions.TextBoxFor(htmlHelper, exampleModel => exampleModel.DifferentProperty, true);
+
+            // Assert
+            Assert.AreEqual("<input "+
+                "data-msg-notequalto=\"These fields cannot match\" "+
+                "data-rule-notequalto=\"{&quot;other&quot;:&quot;#Property&quot;}\" "+
+                "id=\"DifferentProperty\" name=\"DifferentProperty\" type=\"text\" value=\"\" />", result.ToHtmlString());
+        }
+
+    }
+}
