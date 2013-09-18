dbe482650e26231cda75fe630816c1a301bbcfdc
johnnyreilly <johnny_reilly@hotmail.com>
2013-09-18 21:28:21 +0100

    RadioButton tests

---
+++ jVUNUnitTests/RadioButtonExtensionsRequiredTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace jQuery.Validation.Unobtrusive.Native.MVC.UnitTests
+{
+    [TestClass]
+    public class RadioButtonExtensionsRequiredTests
+    {
+        private class RequiredStringModel
+        {
+            [Required]
+            public string String { get; set; }
+        }
+
+        [TestMethod]
+        public void RadioButtonFor_creates_required_data_attributes_for_string_with_Required_attribute()
+        {
+            // Arrange
+            var htmlHelper = HtmlHelperFactory.Create(new RequiredStringModel());
+
+            // Act
+            var result = RadioButtonExtensions.RadioButtonFor(htmlHelper, exampleModel => exampleModel.String, true, "Yes");
+
+            // Assert
+            Assert.AreEqual("<input " +
+                            "data-msg-required=\"The String field is required.\" " +
+                            "data-rule-required=\"true\" " +
+                            "id=\"String\" name=\"String\" type=\"radio\" value=\"Yes\" />", result.ToHtmlString());
+        }
+    }
+}
