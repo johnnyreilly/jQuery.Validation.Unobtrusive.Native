3a0b8017da792ff1a2e1d9e7283c533f60ccfffc
johnnyreilly <johnny_reilly@hotmail.com>
2014-03-31 21:13:32 +0100

    Upgrade to Knockout 3.1 / Switch Bootstrap package

--- jVUNDemo/App_Start/BundleConfig.cs
+++ jVUNDemo/App_Start/BundleConfig.cs
@@ -11,12 +11,15 @@ namespace jQuery.Validation.Unobtrusive.Native.Demos.App_Start
 
             bundles.Add(new ScriptBundle("~/bundles/jquery-validation").Include("~/Scripts/jquery.validate.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/knockout").Include("~/Scripts/knockout-3.0.0.js"));
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include("~/Scripts/knockout-3.1.0.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
         }
     }
