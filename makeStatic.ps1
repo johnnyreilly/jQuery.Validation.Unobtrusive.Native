3e9cc69f49304c62b4a391bf842b652a056f1e3b
John Reilly <johnny_reilly@hotmail.com>
2014-12-16 15:57:51 +0000

    Further tweaks

--- makeStatic.ps1
+++ makeStatic.ps1
@@ -1,20 +1,33 @@
-$jVUNDemo = "$($env:APPVEYOR_BUILD_FOLDER)\jVUNDemo"
-$staticSite = "$jVUNDemo\..\..\"
+param([string]$buildFolder)
+
+$jVUNDemo = "$($buildFolder)\jVUNDemo"
+$staticSiteParent = (get-item $buildFolder).Parent.FullName
+$staticSite = "$($staticSiteParent)\static-site"
 write-host "jVUNDemo location: $jVUNDemo"
+write-host "static site parent location: $staticSiteParent"
 write-host "static site location: $staticSite"
 
 write-host "Spin up jVUNDemo site"
-Start-Job –Name RunIisExpress –Scriptblock {& 'C:\Program Files (x86)\IIS Express\iisexpress.exe' /path:$jVUNDemo /port:57612}
+Start-Job -Name RunIisExpress -Scriptblock {& 'C:\Program Files (x86)\IIS Express\iisexpress.exe' /path:$jVUNDemo /port:57612}
 
+write-host "Wait a moment for IIS to startup"
 Wait-Job -Name RunIisExpress -Timeout 5
 
-write-host "Create static version of demo site here: $staticSite"
-cd $staticSite
+if (Test-Path $staticSite) { 
+    write-host "Removing $($staticSite)..."
+    Remove-Item -path $staticSite -Recurse -Force
+}
+
+write-host "Create static version of demo site here: $($staticSite)"
+Push-Location $staticSiteParent
 wget.exe --recursive --convert-links -E --directory-prefix=static-site --no-host-directories http://localhost:57612/
+Pop-Location
 
 write-host "Shut down jVUNDemo site"
 Receive-Job -Name RunIisExpress
-Get-Job –Name RunIisExpress | Stop-Job
-Get-Job –Name RunIisExpress | Remove-Job
+Get-Job -Name RunIisExpress | Stop-Job
+Get-Job -Name RunIisExpress | Remove-Job
 
-ls $staticSite
+if (Test-Path $staticSite) { 
+    ls $staticSite
+}
