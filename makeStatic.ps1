b56a12b88ad9b2feaa209ca70185fb2b7ae92319
John Reilly <johnny_reilly@hotmail.com>
2014-12-17 11:40:20 +0000

    Increase timeout

--- makeStatic.ps1
+++ makeStatic.ps1
@@ -12,14 +12,14 @@ write-host "static site location: $staticSitePath"
 
 write-host "Spin up jVUNDemo site at $($servedAt)"
 $iisExpressScript = {
-    return & 'C:\Program Files (x86)\IIS Express\iisexpress.exe' /path:$jVUNDemo /port:$port
+    & 'C:\Program Files (x86)\IIS Express\iisexpress.exe' /path:$jVUNDemo /port:$port
 }
 $job = Start-Job -Name RunIisExpress -Scriptblock $iisExpressScript
 
 write-host "Wait a moment for IIS to startup"
-write-host "Job state: $($job.state)"
+write-host "$(Get-Date -format 'u') Job state: $($job.state)"
 Wait-Job $job -Timeout 30
-write-host "Job state: $($job.state)"
+write-host "$(Get-Date -format 'u') Job state: $($job.state)"
 
 if (Test-Path $staticSitePath) { 
     write-host "Removing $($staticSitePath)..."
@@ -35,12 +35,12 @@ wget.exe --recursive --convert-links -E --directory-prefix=$staticSite --no-host
 Pop-Location
 
 write-host "Shut down jVUNDemo site"
-write-host "Job state: $($job.state)"
+write-host "$(Get-Date -format 'u') Job state: $($job.state)"
 Stop-Job $job
 do {
     Start-sleep -s 5
 } while ($job.state -eq "Running")
-write-host "Job state: $($job.state)"
+write-host "$(Get-Date -format 'u') Job state: $($job.state)"
 receive-job $job | out-file jobs.log -append
 cat jobs.log
 Remove-Job $job
