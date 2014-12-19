56f929629d1ec1841239cc427eb477009c80766a
John Reilly <johnny_reilly@hotmail.com>
2014-12-19 11:38:36 +0000

    Update makeStatic.ps1
    Try try try again

--- makeStatic.ps1
+++ makeStatic.ps1
@@ -18,25 +18,27 @@ write-host "Spin up jVUNDemo site at $($servedAt)"
 $process = Start-Process 'C:\Program Files (x86)\IIS Express\iisexpress.exe' -NoNewWindow -ArgumentList "/path:$($jVUNDemo) /port:$($port)"
 
 write-host "Wait a moment for IIS to startup"
-write-host "$(Get-Date -format 'u') Job state: $($job.state)"
-Wait-Job $job -Timeout 30
-write-host "$(Get-Date -format 'u') Job state: $($job.state)"
+Start-sleep -s 15
+#write-host "$(Get-Date -format 'u') Job state: $($job.state)"
+#Wait-Job $job -Timeout 30
+#write-host "$(Get-Date -format 'u') Job state: $($job.state)"
 
 if (Test-Path $staticSitePath) { 
     write-host "Removing $($staticSitePath)..."
     Remove-Item -path $staticSitePath -Recurse -Force
 }
 
-Write-Host "Send request to IIS Express..."
-Invoke-RestMethod $servedAt
+#Write-Host "Send request to IIS Express..."
+#Invoke-RestMethod $servedAt
 
 write-host "Create static version of demo site here: $($staticSitePath)"
 Push-Location $staticSiteParentPath
-wget.exe --recursive --convert-links -E --directory-prefix=$staticSite --no-host-directories --debug $servedAt
+#wget.exe --recursive --convert-links -E --directory-prefix=$staticSite --no-host-directories --debug $servedAt
+wget.exe --recursive --convert-links -E --directory-prefix=$staticSite --no-host-directories $servedAt
 Pop-Location
 
 write-host "Shut down jVUNDemo site"
-write-host "$(Get-Date -format 'u') Job state: $($job.state)"
+#write-host "$(Get-Date -format 'u') Job state: $($job.state)"
 #Stop-Job $job
 #do {
 #    Start-sleep -s 5
