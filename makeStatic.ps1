07eae71a756ff9b8caf2ba9f7cd4f6664623258d
John Reilly <johnny_reilly@hotmail.com>
2014-12-20 14:10:36 +0000

    Update makeStatic.ps1
    learned something

--- makeStatic.ps1
+++ makeStatic.ps1
@@ -10,7 +10,7 @@ $servedAt = "http://localhost:$($port)/"
 write-host "jVUNDemo location: $jVUNDemo"
 write-host "static site parent location: $staticSiteParentPath"
 write-host "static site location: $staticSitePath"
-#write-host "wget log path: $wgetLogPath"
+write-host "wget log path: $wgetLogPath"
 
 write-host "Spin up jVUNDemo site at $($servedAt)"
 $process = Start-Process 'C:\Program Files (x86)\IIS Express\iisexpress.exe' -NoNewWindow -ArgumentList "/path:$($jVUNDemo) /port:$($port)"
@@ -27,10 +27,9 @@ write-host "Create static version of demo site here: $($staticSitePath)"
 Push-Location $staticSiteParentPath
 # 2>&1 used to combine stderr and stdout
 #wget.exe --recursive --convert-links -E --directory-prefix=$staticSite --no-host-directories --debug $servedAt
-#wget.exe --recursive --convert-links -E --directory-prefix=$staticSite --no-host-directories $servedAt > $wgetLogPath 2>&1
-wget.exe --recursive --convert-links -E --directory-prefix=$staticSite --no-host-directories $servedAt 2>&1
+wget.exe --recursive --convert-links -E --directory-prefix=$staticSite --no-host-directories $servedAt > $wgetLogPath 2>&1
 write-host "lastExitCode: $($lastExitCode)"
-#cat $wgetLogPath
+cat $wgetLogPath
 Pop-Location
 
 write-host "Shut down jVUNDemo site"
