cd2db42d96c0fec7e921668bde5079e5d4e2a936
John Reilly <johnny_reilly@hotmail.com>
2014-12-20 14:04:09 +0000

    Update makeStatic.ps1
    Some command line experimentation

--- makeStatic.ps1
+++ makeStatic.ps1
@@ -10,48 +10,30 @@ $servedAt = "http://localhost:$($port)/"
 write-host "jVUNDemo location: $jVUNDemo"
 write-host "static site parent location: $staticSiteParentPath"
 write-host "static site location: $staticSitePath"
-write-host "wget log path: $wgetLogPath"
+#write-host "wget log path: $wgetLogPath"
 
 write-host "Spin up jVUNDemo site at $($servedAt)"
-#$iisExpressScript = {
-#    & 'C:\Program Files (x86)\IIS Express\iisexpress.exe' /path:$jVUNDemo /port:$port
-#}
-#$job = Start-Job -Name RunIisExpress -Scriptblock $iisExpressScript
 $process = Start-Process 'C:\Program Files (x86)\IIS Express\iisexpress.exe' -NoNewWindow -ArgumentList "/path:$($jVUNDemo) /port:$($port)"
 
 write-host "Wait a moment for IIS to startup"
 Start-sleep -s 15
-#write-host "$(Get-Date -format 'u') Job state: $($job.state)"
-#Wait-Job $job -Timeout 30
-#write-host "$(Get-Date -format 'u') Job state: $($job.state)"
 
 if (Test-Path $staticSitePath) { 
     write-host "Removing $($staticSitePath)..."
     Remove-Item -path $staticSitePath -Recurse -Force
 }
 
-#Write-Host "Send request to IIS Express..."
-#Invoke-RestMethod $servedAt
-
 write-host "Create static version of demo site here: $($staticSitePath)"
 Push-Location $staticSiteParentPath
 # 2>&1 used to combine stderr and stdout
 #wget.exe --recursive --convert-links -E --directory-prefix=$staticSite --no-host-directories --debug $servedAt
-wget.exe --recursive --convert-links -E --directory-prefix=$staticSite --no-host-directories $servedAt > $wgetLogPath 2>&1
+#wget.exe --recursive --convert-links -E --directory-prefix=$staticSite --no-host-directories $servedAt > $wgetLogPath 2>&1
+wget.exe --recursive --convert-links -E --directory-prefix=$staticSite --no-host-directories $servedAt 2>&1
 write-host "lastExitCode: $($lastExitCode)"
-cat $wgetLogPath
+#cat $wgetLogPath
 Pop-Location
 
 write-host "Shut down jVUNDemo site"
-#write-host "$(Get-Date -format 'u') Job state: $($job.state)"
-#Stop-Job $job
-#do {
-#    Start-sleep -s 5
-#} while ($job.state -eq "Running")
-#write-host "$(Get-Date -format 'u') Job state: $($job.state)"
-#receive-job $job | out-file jobs.log -append
-#cat jobs.log
-#Remove-Job $job
 stop-process -Name iisexpress
 
 if (Test-Path $staticSitePath) { 
