4786c48b03cbf743dac08d2381114220e9b6a924
johnnyreilly <johnny_reilly@hotmail.com>
2015-02-07 21:59:30 +0000

    upgrade to latest bootstrap

--- jVUNDemo/Scripts/_references.js
+++ jVUNDemo/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="jquery-1.11.2.js" />
-/// <reference path="modernizr-2.7.1.js" />
+﻿/// <reference path="modernizr-2.8.3.js" />
+/// <reference path="jquery-1.9.0.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="knockout-3.0.0.js" />
